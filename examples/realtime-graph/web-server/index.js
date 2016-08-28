80cc590625b5ad6a0c76e493cfcffe79f62971fa
robertvorthman <robert@vorthman.com>
2016-08-27 21:28:30 -0400

    beautified

--- examples/realtime-graph/web-server/index.js
+++ examples/realtime-graph/web-server/index.js
@@ -3,18 +3,18 @@ var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
-io.on('connection', function(socket){
-	//recieve data from smart plug node.js script
-  socket.on('smart plug data', function(msg){
-		//send data to graph
-    io.emit('graph data', msg);
-  });
+io.on('connection', function(socket) {
+    //recieve data from smart plug node.js script
+    socket.on('smart plug data', function(msg) {
+        //send data to graph
+        io.emit('graph data', msg);
+    });
 });
 
-app.get('/', function (req, res) {
-	res.sendFile(__dirname + '/graph.html');
+app.get('/', function(req, res) {
+    res.sendFile(__dirname + '/graph.html');
 });
 
-http.listen(3000, function(){
-	console.log('Node Express Webserver Started, open http://localhost:3000');
+http.listen(3000, function() {
+    console.log('Node Express Webserver Started, open http://localhost:3000');
 });
