e07df2cbc5a513e729ed2d8d09ae53a20b7af789
robertvorthman <robert@vorthman.com>
2016-08-28 08:53:24 -0400

    simplified graph example

---
+++ examples/realtime-graph/index.js
@@ -0,0 +1,33 @@
+var express = require('express');
+var app = express();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+var SmartPlugPowerMonitor = require("smart-plug-power-monitor");
+
+var smartPlugPowerMonitor = new SmartPlugPowerMonitor({
+    smartPlugIP: "192.168.1.104",
+    pollIntervalSeconds: 1,
+    pollingCallback: pollingData,
+});
+
+smartPlugPowerMonitor.start();
+
+var graphSocket;
+//create socket when client connects
+io.on('connection', function(socket) {
+    graphSocket = socket;
+});
+
+function pollingData(powerConsumption) {
+  if(graphSocket) {
+    graphSocket.emit('smart plug data', powerConsumption);
+  }
+}
+
+app.get('/', function(req, res) {
+    res.sendFile(__dirname + '/graph.html');
+});
+
+http.listen(3000, function() {
+    console.log('Node Express Webserver Started, open http://localhost:3000');
+});
